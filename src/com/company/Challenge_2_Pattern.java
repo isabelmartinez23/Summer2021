5cd7f116465237587cd6333ef935312e41c419bb
isabel <isabeltmartinez23>
2020-06-09 14:29:24 -0500

    Initial commit

--- src/com/company/Challenge_2_Pattern.java
+++ src/com/company/Challenge_2_Pattern.java
@@ -18,6 +18,10 @@ public class Challenge_2_Pattern {
         Line();
     }
     public static void Line(){
+        /*
+        This for loop creates the line by first printing a plus,
+        then printing a number of dashes based on the size
+         */
         System.out.print("+");
         for (int dash=0; dash<2*SIZE+1; dash++){
             System.out.print("-");
@@ -26,40 +30,95 @@ public class Challenge_2_Pattern {
     }
 
     public static void TopTriangle(){
+        /*
+        This prints the top triangle
+         */
         for (int row=1; row<=SIZE; row++){
+            /*
+            This statement prints the first vertical bar
+             */
             System.out.print("|");
+            /*
+            This is the loop that prints spaces based on the size
+             */
             for (int space=0; space < SIZE-row+1; space++){
                 System.out.print(" ");
             }
+            /*
+            This is the loop that prints the correct number of backslashes
+            based on the size
+             */
             for (int slash=0; slash < row-1; slash++){
                 System.out.print("/");
             }
+            /*
+            This statement prints the asterisk
+             */
             System.out.print("*");
+            /*
+            This is the loop that prints the correct number of slashes
+            based on the size
+             */
             for (int slash=0; slash < row-1; slash++){
                 System.out.print("\\");
             }
+            /*
+            This is the loop that prints spaces on the right side of the
+            asterisk based on the size
+             */
             for (int space=0; space < SIZE-row+1; space++){
                 System.out.print(" ");
             }
             System.out.println("|");
+            /*
+            This statement prints the last vertical bar
+             */
         }
     }
     public static void BottomTriangle(){
+         /*
+        This prints the bottom triangle
+         */
         for (int row=1; row<=SIZE; row++){
+            /*
+            This statement prints the first vertical bar
+             */
             System.out.print("|");
+            /*
+            This is the loop that prints spaces on the left side of
+            the asterisk based on the size
+             */
             for (int space=0; space < row; space++){
                 System.out.print(" ");
             }
+            /*
+            This is the loop that prints the slashes on the left side
+            of the asterisk based on the size
+             */
             for (int slash=0; slash < SIZE-row; slash++){
                 System.out.print("\\");
             }
+            /*
+            This statement prints the asterisk
+             */
             System.out.print("*");
+            /*
+            This is the loop that prints the slashes on the right side
+            of the asterisk based on the size
+             */
             for (int slash=0; slash < SIZE-row; slash++){
                 System.out.print("/");
             }
+            /*
+            This is the loop that prints the spaces on the right side
+            of the asterisk based on the size
+             */
             for (int space=0; space < row; space++){
                 System.out.print(" ");
             }
+            /*
+            This statement prints the last vertical bar
+             */
             System.out.println("|");
         }
     }
