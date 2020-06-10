87c37d39f7a4d617c5b60816d54e095c12ef728d
isabel <isabeltmartinez23>
2020-06-10 16:08:59 -0500

    Initial commit

--- src/com/company/Gangsta_Name_V2.java
+++ src/com/company/Gangsta_Name_V2.java
@@ -4,14 +4,35 @@ import java.util.Scanner;
 public class Gangsta_Name_V2 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Type your first name, playa:");
-        String firstName = input.nextLine();
-        System.out.println("Type your last name, playa:");
-        String lastName = input.nextLine();
-        System.out.println(firstName + lastName);
-        char letter = firstName.charAt(0);
-        System.out.println(letter + ". Diddy " + lastName.toUpperCase() + " " + firstName + "-izzle");
+        System.out.println("Type your full name, playa:");
+        String fullName = input.nextLine();
+        Challenge_2_Pattern.Line();
+        System.out.println("Your gangsta name is: ");
+        printName(fullName);
+    }
+    public static void printName(String fullName){
+        firstInitial(fullName);
+        System.out.print(". Diddy");
+        lastName(fullName);
+        firstName(fullName);
+        System.out.print("-izzle");
 
-
+    }
+    public static void lastName(String fullName){
+        int indexOf = fullName.indexOf(" ");
+        if (indexOf>0){
+            String lastName = fullName.substring(indexOf);
+            lastName = lastName.toUpperCase();
+            System.out.print(lastName);
+        }
+    }
+    public static void firstName(String fullName){
+        int indexOf = fullName.indexOf(" ");
+        String firstName = fullName.substring(0, indexOf);
+        System.out.print(" " + firstName);
+    }
+    public static void firstInitial(String fullName){
+        char initial = fullName.charAt(0);
+        System.out.print(initial);
     }
 }
