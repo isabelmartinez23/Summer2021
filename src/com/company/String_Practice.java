071893063b2bc60ea42ecb55a161bcdf20a18ac0
isabel <isabeltmartinez23>
2020-06-13 10:42:48 -0500

    Initial commit

--- src/com/company/String_Practice.java
+++ src/com/company/String_Practice.java
@@ -1,13 +1,54 @@
 package com.company;
 import java.util.Scanner;
-
 public class String_Practice {
     public static Scanner Keyboard = new Scanner(System.in);
-    public static void main (String[] args){
+    public static void main(String[] args) {
         System.out.print("Type any phrase: ");
         String phrase = Keyboard.nextLine();
-    }
-    public static void counter(String phrase){
+        int phraseLength = phrase.length();
+        // phrase = phrase - firstWord
+        int indexOf = phrase.indexOf(" ");
 
+        String firstWord = phrase.substring(0, indexOf+1);
+        System.out.println(firstWord);
+        char character = ' ';
+        int charCount = 0;
+
+        for (int index = 0; index < firstWord.length(); index++) {
+            character = firstWord.charAt(index);
+            if (character != ' ') {
+                charCount++;
+                String temp = phrase.substring(index + 1);
+                indexOf = temp.indexOf(character);
+                while (indexOf >= 0) {
+                    charCount++;
+                    temp = temp.substring(indexOf + 1);
+                    indexOf = temp.indexOf(character);
+                }
+                //charCount = restProg(character, phrase, charCount);
+                firstWord = firstWord.replace(character, ' ');
+                System.out.println(character + " " + charCount);
+                charCount = 0;
+            }
+        }
+        firstWord = phrase.substring(0, phrase.indexOf(" "));
+        for (int index = 0; index < firstWord.length(); index++){
+            character = firstWord.charAt(index);
+            if (character != ' ') {
+                charCount = restProg(character, phrase, 0);
+                System.out.println(character + " " + charCount);
+            }
+        }
+    }
+    public static int restProg(char character, String phrase, int charCount){
+        int indexOf = phrase.indexOf(character);
+        charCount = 0;
+        while (indexOf >= 0) {
+            charCount++;
+            phrase = phrase.substring(indexOf + 1);
+            indexOf = phrase.indexOf(character);
+        }
+        return charCount;
     }
 }
+
