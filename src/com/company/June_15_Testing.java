9bdc2bff16f6bae1249e3dbd6e2caf1918869d66
isabel <isabeltmartinez23>
2020-06-16 13:15:21 -0500

    June 16th update

--- src/com/company/June_15_Testing.java
+++ src/com/company/June_15_Testing.java
@@ -9,20 +9,12 @@ public class June_15_Testing {
     }
     public static void demonstration(){
         Scanner input = new Scanner(System.in);
-        String answer;
-        Random randy = new Random();
+        random();
         double double1 = 9.98;
         double double2 = double1 * 3.234234;
         double double3 = double2 / 2.345345;
         int integer1 = 345;
         int integer2 = -58;
-        do {
-            int rand = randy.nextInt(100) +750;
-            System.out.printf("My random number is %5d \n", rand);
-            System.out.print("Do you want to quit? (Yes/No): ");
-            answer = input.next();
-        }
-        while (answer.equals("No")) ;
         String whatever = "Greenhill is awesome";
         System.out.printf("Showing how to use printf \n $%10.3f \n $%10.3f \n $%10.3f " +
                 "\n -%6d \n -%6d \n %20.30s"
@@ -35,5 +27,17 @@ public class June_15_Testing {
         double double3 = double2/Math.random();
 
         System.out.printf("\n %10.3f \n %10.3f \n %10.3f",double1, double2, double3);
+    }
+    public static void random (){
+        Scanner input = new Scanner(System.in);
+        Random randy = new Random();
+        String answer;
+        do {
+            int rand = randy.nextInt(100) +750;
+            System.out.printf("My random number is %5d \n", rand);
+            System.out.print("Do you want to quit? (Yes/No): ");
+            answer = input.next();
+        }
+        while (answer.equals("No")) ;
     }
 }
