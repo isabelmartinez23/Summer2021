1d39a43d28afa73c4aaf3840364a72defe2d9c98
isabel <isabeltmartinez23>
2020-06-16 15:52:33 -0500

    June 16th update

--- src/com/company/Challenge_Data_Types.java
+++ src/com/company/Challenge_Data_Types.java
@@ -1,6 +1,24 @@
 package com.company;
 import java.util.Scanner;
 
+/*
+Isabel Martinez
+PseudoCode
+Import Scanner
+Create the Scanner object
+Create three variables, one int, one double and one String
+Ask the user for for an integer
+Use input.nextInt to initialize variable
+Ask the user for a double
+Use input.nextDouble to initialize variable
+Ask the user for a string
+Use input.nextLine to initialize variable
+Create a new variable that is the first inputed integer + the first variable
+Create a new variable that is the second inputed double + the double i created
+Create a third variable that is the string I created concatenated with the string they created
+Print all of these using printf so it is formatted correctly 
+ */
+
 public class Challenge_Data_Types {
     public static void main (String[] args){
         Scanner input = new Scanner(System.in);
@@ -9,21 +27,18 @@ public class Challenge_Data_Types {
         String s = "Greenhill ";
 
         System.out.print("Type any integer: ");
-        String integerStr = input.nextLine();
+        int myInt = input.nextInt();
         System.out.print("Type any double: ");
-        String doubleStr = input.nextLine();
+        double myDouble = input.nextDouble();
         System.out.print("Type any string: ");
-        String string1 = input.nextLine();
+        String myString = input.nextLine();
 
-        int myInt = Integer.parseInt(integerStr);
-        double myDouble = Double.parseDouble(doubleStr);
 
         int first = i + myInt;
         double second = d + myDouble;
-        String third = s + string1;
+        String third = s + myString;
 
         System.out.printf("\n %8.3s \n %10.6s \n %10.50s", first, second, third);
-        
     }
 
 }
