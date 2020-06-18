d230f25b9e34cfbe91f4ba2a91b2fd1e76c84300
isabel <isabeltmartinez23>
2020-06-17 19:25:47 -0500

    June 17th update

--- src/com/company/Challenge_Operators.java
+++ src/com/company/Challenge_Operators.java
@@ -1,6 +1,25 @@
 package com.company;
 import java.util.Scanner;
-
+/*
+Isabel Martinez
+PseudoCode
+Import Scanner
+Create scanner object
+Print "Type the meal cost"
+Use nextDouble to initialize variable for the meal cost
+Print "Type the tip percent"
+Use nextInt to initialize variable for the tip percent
+Print "Type the tax percent"
+Use nextInt to initialize variable for the tax percent
+Create new double tipRate and multiple the tipPercent by .01
+Create new double taxRate and multiple the taxPercent by .01
+Create new double totalTip which is mealCost*tipRate
+Create new double totalTax which is mealCost*taxRate
+Create new double totalCost which is mealCost * totalTip * totalTax
+Use Math.round to round total cost to the nearest whole number
+Print Line from Challenge 2 to make clear
+Print totalCost with printf to make sure there are 0 decimal places after the number
+ */
 
 public class Challenge_Operators {
     public static Scanner input = new Scanner(System.in);
@@ -8,17 +27,25 @@ public class Challenge_Operators {
 
         System.out.print("Type the meal cost: ");
         double mealCost = input.nextDouble();
-        System.out.print("Type any double: ");
+        System.out.print("Type the tip percent: ");
         int tipPercent = input.nextInt();
-        System.out.print("Type any string: ");
+        System.out.print("Type tax percent: ");
         int taxPercent = input.nextInt();
 
-        mealCost = Math.round(mealCost);
-        double totalTip = mealCost*(Math.floorDiv(tipPercent, 100));
-        double totalTax = mealCost*(Math.floorDiv(taxPercent, 100));
+        double tipRate = tipPercent*.01;
+        double taxRate = taxPercent*.01;
+
+        double totalTip = mealCost*tipRate;
+        double totalTax = mealCost*taxRate;
         double totalCost = mealCost + totalTip + totalTax;
 
-        System.out.print(totalCost);
+        totalCost = Math.round(totalCost);
+
+        Challenge_2_Pattern.Line();
+        System.out.print("Total Cost: ");
+        System.out.printf("%1.0f", totalCost);
+
+
 
     }
 }
