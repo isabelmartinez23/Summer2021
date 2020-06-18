09d4c3ef0d6d685a61555ea2a00be4e057fc1c69
isabel <isabeltmartinez23>
2020-06-18 08:28:57 -0500

    June 18th update

--- src/com/company/Challenge_Operators.java
+++ src/com/company/Challenge_Operators.java
@@ -11,14 +11,16 @@ Print "Type the tip percent"
 Use nextInt to initialize variable for the tip percent
 Print "Type the tax percent"
 Use nextInt to initialize variable for the tax percent
-Create new double tipRate and multiple the tipPercent by .01
-Create new double taxRate and multiple the taxPercent by .01
-Create new double totalTip which is mealCost*tipRate
-Create new double totalTax which is mealCost*taxRate
-Create new double totalCost which is mealCost * totalTip * totalTax
-Use Math.round to round total cost to the nearest whole number
-Print Line from Challenge 2 to make clear
-Print totalCost with printf to make sure there are 0 decimal places after the number
+Create new method to do the calculations:
+    Create new double tipRate and multiple the tipPercent by .01
+    Create new double taxRate and multiple the taxPercent by .01
+        Create new double totalTip which is mealCost*tipRate
+    Create new double totalTax which is mealCost*taxRate
+    Create new double totalCost which is mealCost * totalTip * totalTax
+    Use Math.round to round total cost to the nearest whole number
+    Print Line from Challenge 2 to make clear
+    Print totalCost with printf to make sure there are 0 decimal places after the number
+In the main, call the method that does the equations 
  */
 
 public class Challenge_Operators {
@@ -32,6 +34,10 @@ public class Challenge_Operators {
         System.out.print("Type tax percent: ");
         int taxPercent = input.nextInt();
 
+        solve(mealCost,tipPercent,taxPercent);
+
+    }
+    public static void solve (double mealCost, int tipPercent, int taxPercent){
         double tipRate = tipPercent*.01;
         double taxRate = taxPercent*.01;
 
@@ -44,8 +50,6 @@ public class Challenge_Operators {
         Challenge_2_Pattern.Line();
         System.out.print("Total Cost: ");
         System.out.printf("%1.0f", totalCost);
-
-
 
     }
 }
