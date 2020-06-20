54a8c66eb7040a9bde8c09b94ae9a490a8e743ff
isabel <isabeltmartinez23>
2020-06-20 12:44:21 -0500

    June 20th update

--- src/com/company/Challenge_String.java
+++ src/com/company/Challenge_String.java
@@ -1,5 +1,6 @@
 package com.company;
 import java.util.Scanner;
+import java.util.Arrays;
 /*
 Isabel Martinez
 PseudoCode
@@ -19,13 +20,44 @@ Return and print in main
 
 public class Challenge_String {
     public static Scanner input = new Scanner(System.in);
-    public static void main (String[] args){
-        System.out.print("How many words will you type? ");
-        int numberWords = input.nextInt();
-        for (int words = 0; words <=numberWords; words++){
-            System.out.println("Type your word: ");
-            String word1 = input.nextLine();
+
+    public static void main(String[] args) {
+        int numberWords;
+        do {
+            System.out.print("How many words will you type? ");
+            numberWords = input.nextInt();
         }
+        while (numberWords < 1 || numberWords > 10);
+        String[] inputWords = new String[numberWords];
+        String word1 = input.nextLine();
+        for (int wordCount = 0; wordCount < numberWords; wordCount++) {
+            do {
+                System.out.print("Type your word: ");
+                inputWords[wordCount] = input.nextLine();
+            }
+            while (inputWords[wordCount].length() < 2 || inputWords[wordCount].length() >10000);
+        }
+        for (int wordCount = 0; wordCount < numberWords; wordCount++){
+            System.out.print(even(inputWords[wordCount]));
+            System.out.print(" ");
+            System.out.println(odd(inputWords[wordCount]));
+        }
+    }
+
+    public static String even(String word1) {
+        String evenLetters = "";
+        for (int evenIndex = 0; evenIndex < word1.length(); evenIndex = evenIndex + 2) {
+            evenLetters = evenLetters + word1.charAt(evenIndex);
+        }
+        return evenLetters;
+    }
+
+    public static String odd(String word1) {
+        String oddLetters = "";
+        for (int oddIndex = 1; oddIndex < word1.length(); oddIndex = oddIndex + 2) {
+            oddLetters = oddLetters + word1.charAt(oddIndex);
+        }
+        return oddLetters;
     }
 }
 
