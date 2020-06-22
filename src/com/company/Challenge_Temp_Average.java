3fe7abd715229ed15a9f7fdb73a5e42fadf15f27
isabel <isabeltmartinez23>
2020-06-22 17:56:43 -0500

    June 22st update

--- src/com/company/Challenge_Temp_Average.java
+++ src/com/company/Challenge_Temp_Average.java
@@ -23,29 +23,39 @@ Print "The number of days above average was " + counter + " days"
 public class Challenge_Temp_Average {
     public static Scanner input = new Scanner(System.in);
     public static void main(String[] args){
+        // this initializes the variable
         int numberDays;
+        // this asks for the number of temperatures, from 2 to 100
         do {
             System.out.print("How many temperatures? ");
             numberDays = input.nextInt();
         }
         while (numberDays < 2 || numberDays > 100);
+        // this creates an array to hold the temperatures
         int [] temps = new int[numberDays];
+        // this creates a random
         Random randy = new Random();
+        // this initializes the sum
         int sum = 0;
+        // this loop prints random temperatures and adds them to the sum
         for (int index = 0; index < temps.length; index++){
             temps[index] = randy.nextInt(85) + 25 ;
             System.out.println("Day " + (index + 1)+ " high temp: " + temps[index]);
             sum = sum + temps[index];
         }
+        // this creates the average
         double average = (double) sum/numberDays;
+        // this prints the average
         System.out.println("The average of all temperatures is: " + average);
+        // this is the counter for how many days over average
         int counter = 0;
+        // this adds one each time a temperature is over average
         for (int index =0; index < temps.length; index++){
             if (temps[index] > average){
                 counter = counter + 1;
             }
         }
+        // this prints how many days were above average
         System.out.println(counter + " days were above average!");
-
     }
 }
