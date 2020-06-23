26de795d90ca3837f7aeef6aa27f996802f93f94
isabel <isabeltmartinez23>
2020-06-23 14:40:16 -0500

    June 23st update

--- src/com/company/Array_Practice_2.java
+++ src/com/company/Array_Practice_2.java
@@ -7,9 +7,20 @@ public class Array_Practice_2 {
             System.out.println("The array " + Arrays.toString(numbers) + " has " + len + " elements");
 
             Random rand = new Random();
-            int [] numbers2 = new int[10];
+            int [] numbers2 = new int[5];
             for (int i =0; i < numbers2.length; i++){
-                numbers2[i] = rand.nextInt(70) + 20; 
+                numbers2[i] = rand.nextInt(70) + 20;
             }
+            System.out.println(Arrays.toString(numbers2));
+            numbers2 = reverse(numbers2);
+            System.out.println(Arrays.toString(numbers2));
+        }
+        public static int[] reverse(int [] array){
+            for (int i = 0; i < array.length/2; i++){
+                int temp = array[i];
+                array [i] = array[array.length-1-i];
+                array[array.length-1-i] = temp;
+            }
+            return array;
         }
 }
