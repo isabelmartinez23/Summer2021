63b352261aade23e5ca83fd9f48ff524304037be
isabel <isabeltmartinez23>
2020-06-23 17:06:10 -0500

    June 23rd update

--- src/com/company/ClassWork_6_23.java
+++ src/com/company/ClassWork_6_23.java
@@ -22,35 +22,50 @@ Print the final array
  */
 public class ClassWork_6_23 {
     public static void main(String[] args){
+        // this initializes the random variable
         Random rand1 = new Random();
+        // this makes the size variable between 5 and 10
         int size = rand1.nextInt(5)+5;
+        // this makes the array the size of 'size'
         int [] array1 = new int[size];
+        // this initializes index 1
         int index1 ;
-        int index2 ; 
+        // this initializes index2
+        int index2 ;
 
+        // this makes sure index 1 and 2 are not the same random integers
         do{
             index1 = rand1.nextInt(size);
             index2 = rand1.nextInt(size);
         }
         while (index1 == index2);
 
+        // this prints index 1 and 2
         System.out.println(index1);
         System.out.println(index2);
 
+        // this adds random integers between 1 and 99 to the array
         for (int i = 0; i < array1.length; i++){
             array1 [i] = rand1.nextInt(98) + 1;
         }
+        // this prints the array before
         System.out.println(Arrays.toString(array1));
+        // this calls the swap method
         array1 = swap(array1, index1, index2);
+        // this prints the array after
         System.out.println(Arrays.toString(array1));
 
 
     }
     public static int[] swap (int[] array1, int index1, int index2){
+        // this creates a temp variable and makes it equal to array at index1
         int temp = array1 [index1];
+        // this makes array at index 1 equal to array at index 2
         array1 [index1] = array1 [index2];
+        // this makes array at index 2 equal to temp
         array1 [index2] = temp;
 
+        // this returns array back to the main 
         return array1;
     }
 }
